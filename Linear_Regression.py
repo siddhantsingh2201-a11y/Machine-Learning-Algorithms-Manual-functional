bcd75732af72dc28d77f47654e4a2746fa02df52
siddhantsingh2201-a11y <siddhantsingh2201@gmail.com>
2026-01-05 10:03:32 +0530

    Linear regression learning 1

---
+++ Linear_Regression.py
@@ -0,0 +1,8 @@
+"""
+Writing the Linear Regression Core Code. Manual Implementation and Library implementation.
+
+Line Function : y = mx + c.
+MSE Error Function : 1/n * {[summation(x-x_mean) * (y - y_mean)]/summation(x-x_mean)}
+ In line function : m = Sum()
+
+"""
